12d94e75f8fdf3c36ad6a5caa40eede7e2de4f73
Jacob Fuehne <Fuehnix@users.noreply.github.com>
2020-03-16 05:19:27 -0500

    removed unnecessary variable, replaced tag -> permutation
    replaced tag variable with permutation for more clear naming,  removed length variable as it was unused

--- src/solid/spintax/spinner/SolidSpintax/SolidSpintaxSwitch.java
+++ src/solid/spintax/spinner/SolidSpintax/SolidSpintaxSwitch.java
@@ -35,18 +35,16 @@ public class SolidSpintaxSwitch implements SolidSpintaxElement {
     }
 
     @Override
-    public String spin(BigInteger tag) {
-        int length = children.size();
-        //absolute int range
+    public String spin(BigInteger permutation) {
         for (int i = 0; i < children.size(); i++) {
             BigInteger curPermutations = children.get(i).countPermutations();
-            if (tag.compareTo(curPermutations) < 0) {
-                return children.get(i).spin(tag);
+            if (permutation.compareTo(curPermutations) < 0) {
+                return children.get(i).spin(permutation);
             } else {
-                tag = tag.subtract(curPermutations);
+                permutation = permutation.subtract(curPermutations);
             }
         }
-        System.out.println("Error: tag not reached");
+        System.out.println("Error: permutation not reached");
         return "ERROR";
     }
 
